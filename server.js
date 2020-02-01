1412f037f53aeae7257aba00bfebba01109a505e
APaulaMiranda <apaulafmiranda@gmail.com>
2020-02-01 17:26:22 +0100

    add post request to create a workout

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require('path');
 
 const db = require("./models");
 
@@ -14,27 +15,32 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/api/workouts", ({ body }, res) => {
+app.get("/api/workouts", (_, res) => {
     db.Workout.find({}).then(result => res.json(result));
 });
 
-app.get("/api/workouts/:id", ({ body, params }, res) => {
-    db.Workout.findById(params.id).then(result => res.json(result));
+app.get("/api/workouts/:id", (req, res) => {
+    db.Workout.findById(req.params.id).then(result => res.json(result));
 });
 
-app.put("/api/workouts/:id", ({ params, body }, res) => {
-    db.Workout.findById(params.id).then(workout => {
-        if (workout !== null) {
-            workout.exercises.push(body);
-            db.Workout.updateOne({ _id: params.id }, workout);
-            workout.save().then(() => res.json(workout));
-        }
-    })
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, { exercises: req.body })
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        }).catch(err => res.json(err));
 });
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body).then(dbTransaction => {
+        res.json(dbTransaction);
+    }).catch(err => res.json(err));
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
